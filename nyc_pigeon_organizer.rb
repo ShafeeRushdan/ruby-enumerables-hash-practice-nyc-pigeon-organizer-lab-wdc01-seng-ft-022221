41e2377ff31c387bc3f96e37b4fffb8171af15cf
Shafee Rushdan <shafeerushdan@yahoo.com>
2021-02-17 04:21:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
 
 data.each_with_object ({}) do |(key,value),final_array|
 
-value.each do (inner_key) |
+value.each do |(inner_key),names|
   # write your code here!
 end
 final_array
